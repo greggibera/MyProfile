6e02e8cae46842587b8ff6d49990aea96e669dc7
Gregg Ibera Ibera <igreggvictor@yahoo.com>
2023-06-19 17:36:46 -0600

    major update

--- js/script.js
+++ js/script.js
@@ -113,3 +113,28 @@ const typed= new Typed('.multiple-text', {
   loop:true
 });
 
+
+
+function addPictures(){
+
+  var x = document.getElementById("more");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  var y = document.getElementById("viewMore");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+
+  var z = document.getElementById("btn-goBack");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+}
